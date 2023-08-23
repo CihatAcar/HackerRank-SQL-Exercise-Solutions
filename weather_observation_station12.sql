92bdea488eb2fc38d0408e5849d019bd228797c9
Cihat Acar <cihat.acar4182@gmail.com>
2023-08-23 16:03:41 +0200

    Weather observation station 13 exercise solution is added.

--- weather_observation_station12.sql
+++ weather_observation_station12.sql
@@ -1,5 +1,5 @@
 /*
-Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.GIT
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 
 The STATION table is described as follows:
  ID: NUMBER
