e69c32f22ec9d1027a23ea96adc9394e057c6132
Cihat Acar <cihat.acar4182@gmail.com>
2023-08-23 15:47:50 +0200

    Employee salaries exercise solution is added.

--- employee_salaries.sql
+++ employee_salaries.sql
@@ -1,5 +1,7 @@
 /*
-Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+Write a query that prints a list of employee names (i.e.: the name attribute)
+for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months.
+Sort your result by ascending employee_id.
 
 The Employee table containing employee data for a company is described as follows:
  employee_id: Integer
@@ -12,4 +14,5 @@ months is the total number of months they've been working for the company, and s
 
 SELECT name
 FROM Employee
-ORDER BY name;
+WHERE salary > 2000 and months < 10
+ORDER BY employee_id;
