042a50f6390c3fda07443f1dd6858adcc3ac3582
Cihat Acar <cihat.acar4182@gmail.com>
2023-08-23 13:04:13 +0200

    Weather observation station 9 exercise solution is added.

---
+++ weather_observation_station9.sql
@@ -0,0 +1,16 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
+ ID: NUMBER
+ CITY: VARCHAR2(21)
+ STATE: VARCHAR2(2)
+ LAT_N: NUMBER
+ LONG_W: NUMBER
+Where LAT_N is the northern latitude and LONG_W is the western longitude.
+ */
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE
+    UPPER(SUBSTR(CITY, 1, 1)) NOT IN ("A", "E", "I", "O", "U")
