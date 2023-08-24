c06b52eda436756b39e4089ef8f7b1b14f6e9dfe
Cihat Acar <cihat.acar4182@gmail.com>
2023-08-24 13:36:30 +0200

    Population density difference exercise solution is added.

---
+++ population_density_diffference.sql
@@ -0,0 +1,12 @@
+/*
+Query the difference between the maximum and minimum populations in CITY.
+The CITY table is described as follows:
+ ID: NUMBER
+ NAME: VARCHAR2(17)
+ COUNTRYCODE: VARCHAR2(3)
+ DISTRICT: VARCHAR2(20)
+POPULATION: NUMBER
+ */
+
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY;
