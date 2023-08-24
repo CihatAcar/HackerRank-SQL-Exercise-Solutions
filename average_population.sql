0c524c9e5b7344392568deb61a8cc5d6c4187c7e
Cihat Acar <cihat.acar4182@gmail.com>
2023-08-24 13:32:26 +0200

    Average population exercise solution is added.

---
+++ average_population.sql
@@ -0,0 +1,12 @@
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+The CITY table is described as follows:
+ ID: NUMBER
+ NAME: VARCHAR2(17)
+ COUNTRYCODE: VARCHAR2(3)
+ DISTRICT: VARCHAR2(20)
+POPULATION: NUMBER
+ */
+
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
